135c8d09d5496052369939d126233439c7920bb2
Nathan Soares <nathannss14@gmail.com>
2022-11-12 19:20:39 -0300

    Revert "Dia 02"
    This reverts commit f6b8faf087d0bba878c3077f8efa5deb2e3d8a2d.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,6 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors'
 import { prisma } from './prisma';
-import { z } from "zod";
-import { randomCode } from '../utils';
 
 const port = 3333 
 
@@ -14,37 +12,9 @@ async function start(){
     await fastify.register(cors,{
         origin:true
     })
-    //////////////Rotas//////////////
+
     fastify.get('/pools/count', async ()=>{
-        const count = await prisma.pool.count()
-        return {count}
-    })
-
-    fastify.get('/users/count', async ()=>{
-        const count = await prisma.user.count()
-        return {count}
-    })
-
-    fastify.get('/guesses/count', async ()=>{
-        const count = await prisma.guess.count()
-        return {count}
-    })
-
-    fastify.post('/pools', async (req, res)=>{
-        const bodyReq = z.object({
-            title: z.string()
-        });
-
-        const {title} = bodyReq.parse(req.body)
-        const code = randomCode(6).toUpperCase()
-        await prisma.pool.create({
-            data:{
-                title:title,
-                code: code
-            }
-        })
-
-        return res.status(201).send({code})
+        return prisma.pool.count()
     })
 
     await fastify.listen({port: port/*, host: "0.0.0.0"*/})
